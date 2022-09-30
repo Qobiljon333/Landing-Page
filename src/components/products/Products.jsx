ea431f01730abcc7cf22ef474dc46a9ad6d8614f
Qobiljon333 <95879507+Qobiljon333@users.noreply.github.com>
2022-09-30 10:17:02 +0500

    product border solve

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -9,7 +9,7 @@ const Products = () => {
         <div className="flex flex-wrap mt-[110px]    w-full">
           {
               PRODUCTS.map(({name,svg,svg2},inx)=>(
-                <div onMouseLeave={()=> setState("")} onMouseEnter={()=>setState(name)} className={` ${inx > 4 && inx < 9 ? "lg:border-r-2 lg:  border-b-0":""} ${inx < 5 && inx < 4 ? "lg:border-b-2 lg:border-r-2":""} ${ inx === 4 ? "lg:border-b-2" :"" } ${inx == 9 ? "lg:border-b-0" :""} border-b-2 cursor-pointer  w:1/2 lg:w-1/5 text-white hover:bg-white   hover:text-black px-7 py-16 d  border-white border-solid `} key={inx}> 
+                <div onMouseLeave={()=> setState("")} onMouseEnter={()=>setState(name)} className={` ${inx > 4 && inx < 9 ? "lg:border-r-2   ":""} ${inx > 4 && inx < 9 ? "lg:border-r-2":""} ${ inx === 4 ? "lg:border-b-2" :"" } ${inx == 9 ? "lg:border-b-0" :""} border-b-2 cursor-pointer  w:1/2 lg:w-1/5 text-white hover:bg-white   hover:text-black px-7 py-16 d  border-white border-solid `} key={inx}> 
                 { state ==name ? svg2 :svg}
                 <h1 className='lg:text-lg '>{name}</h1>
                 </div>
